e45eae6317be18f23d6d56605c2be69fce02c050
mostofa kamal <mostofakamal1990@gmail.com>
2022-02-25 21:31:59 +0600

    object loops for in for of and object keys, object entries

---
+++ object-loops.js
@@ -0,0 +1,28 @@
+const studenst = { name: "kamal", age: 66, address: "gazipur", isCliend: true };
+
+// for(let i =0; i<10; i++){
+//     console.log(i);
+// }
+
+
+for(const stud in studenst){
+
+  //  console.log(stud,' = ', studenst[stud]);
+}
+
+
+const studenstKeys = Object.keys(studenst);
+
+console.log(studenstKeys);
+
+for(const prop of studenstKeys){
+   // console.log(studenst[prop]);
+}
+
+
+// object entries Advance
+const objectEntries = Object.entries(studenst);
+
+for(const [key, value] of objectEntries){
+    console.log(key , ' = ', value);
+}
