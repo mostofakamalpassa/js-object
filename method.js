91d4f4ec776359eb4e49961745670010ae843e46
mostofa kamal <mostofakamal1990@gmail.com>
2022-02-25 20:54:09 +0600

    update Propery and menthod

--- method.js
+++ method.js
@@ -1,5 +1,6 @@
 const studenst = {
     id:55,
+    balance: 6000,
     name:'kamal passa',
     major:'Economices',
     subjects: ['English','socail work','statestics','Math','micro Economics', 'demographi','others'],
@@ -8,7 +9,15 @@ const studenst = {
     takeExmap: function(){
         
         console.log('Taking Exam '+ this.name);
+    },
+    cost(balance){
+        
+        const remaningBalance = this.balance - balance;
+        console.log(remaningBalance);
+
     }
 }
 
 studenst.takeExmap();
+
+studenst.cost(1000);
